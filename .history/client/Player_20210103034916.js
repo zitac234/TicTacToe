cad86667d17fa4760069a74c43e1e01611623faf
Nneoma Chukwukelu <zitac234@gmail.com>
2021-01-03 21:49:59 -0500

    can accept and place mark

---
+++ .history/client/Player_20210103034916.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Form from "./Form";
+import Board from "./Board";
+import { Link } from "react-router-dom";
+export default class Player extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      mark: "",
+      showGame: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  componentDidMount() {
+    document.getElementById("myboard").style.backgroundColor = "blue";
+  }
+  componentW
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+  handleSubmit(event) {
+    this.setState({
+      showGame: true,
+    });
+    document.getElementById("myform").style.display = "none";
+    document.getElementById("myboard").style.backgroundColor = "blue";
+  }
+  render() {
+    const myForm = (
+      <Form
+        {...this.state}
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+      />
+    );
+    return (
+      <div>
+        {this.state.showGame ? (
+          <div id="myboard">
+            <Board />{" "}
+          </div>
+        ) : (
+          <div id="myform">{myForm}</div>
+        )}
+      </div>
+    );
+  }
+}
---
+++ .history/client/Player_20210103035344.js
@@ -0,0 +1,53 @@
+import React, { Component } from "react";
+import Form from "./Form";
+import Board from "./Board";
+import { Link } from "react-router-dom";
+export default class Player extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      mark: "",
+      showGame: false,
+    };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  componentDidMount() {
+    document.getElementsByTagName("body")[0].style.backgroundColor = "white";
+  }
+  handleChange(event) {
+    event.preventDefault();
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+  handleSubmit(event) {
+    this.setState({
+      showGame: true,
+    });
+    document.getElementById("myform").style.display = "none";
+    document.getElementsByTagName("body")[0].style.backgroundColor =
+      "blanchedalmond";
+  }
+  render() {
+    const myForm = (
+      <Form
+        {...this.state}
+        handleChange={this.handleChange}
+        handleSubmit={this.handleSubmit}
+      />
+    );
+    return (
+      <div>
+        {this.state.showGame ? (
+          <div id="myboard">
+            <Board />{" "}
+          </div>
+        ) : (
+          <div id="myform">{myForm}</div>
+        )}
+      </div>
+    );
+  }
+}
