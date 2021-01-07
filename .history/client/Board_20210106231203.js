9ed870153a8d744ed99dda8f19e05f3b4c966205
Nneoma Chukwukelu <zitac234@gmail.com>
2021-01-07 01:18:45 -0500

    Computer is Workin

---
+++ .history/client/Board_20210106231203.js
@@ -0,0 +1,144 @@
+import React, { Component } from "react";
+import Computer from "./Computer";
+const table = document.createElement("table");
+export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      grid: table,
+      PlayerMark: "O",
+      ComputerMark: "X",
+    };
+    this.placeMark = this.placeMark.bind(this);
+  }
+  componentDidMount() {
+    this.placeMark();
+  }
+
+  emptyCell(index) {
+    let cell;
+    if (index) {
+      let [r, c] = [index[0], index[1]];
+      cell = table.rows[r].cells[c];
+      return cell.innerHTML === "" ? true : false;
+    } else {
+      for (let i = 0; i < 3; i++) {
+        for (let n = 0; n < 3; n++) {
+          cell = table.rows[i].cells[n];
+          if (cell.innerHTML === "") {
+            return true;
+          }
+        }
+        return false;
+      }
+    }
+  }
+
+  rowWin(mark) {
+    for (let i = 0; i < 3; i++) {
+      for (let n = 0; n < 3; n++) {
+        const cell = table.rows[i].cells[n];
+        if (cell.innerHTML !== mark) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+  updateTable(myGrid) {
+    for (let i = 0; i < 3; i++) {
+      for (let n = 0; n < 3; n++) {
+        const grid = myGrid.rows[i].cells[n];
+        const cell = this.state.grid.rows[i].cells[n];
+        if (grid.innerHTML !== cell.innerHTML) cell.innerHTML = grid.innerHTML;
+      }
+    }
+    console.log('this is myGrid')
+  }
+  colWin(mark) {
+    for (let i = 0; i < 3; i++) {
+      for (let n = 0; n < 3; n++) {
+        const cell = table.rows[n].cells[i];
+        if (cell.innerHTML !== mark) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+  diagWin(mark) {
+    let n = 2;
+    for (let i = 0; i < 3; i++) {
+      const positiveCell = table.rows[i].cells[i];
+      const negativeCell = table.rows[i].cells[n];
+      if (positiveCell.innerHTML !== mark && negativeCell.innerHTML !== mark) {
+        return false;
+      }
+      n--;
+    }
+    return true;
+  }
+  win(mark) {
+    if (this.diagWin(mark) || this.colWin(mark) || this.rowWin(mark)) {
+      return true;
+    }
+  }
+
+  switchTurn(mark) {
+    if (mark === this.state.PlayerMark) {
+      return this.state.ComputerMark;
+    } else {
+      return this.state.PlayerMark;
+    }
+  }
+  placeMark() {
+    let mark = this.state.PlayerMark;
+    table.addEventListener("click", (event) => {
+      const cellId = event.target.id;
+      const cell = document.getElementById(cellId);
+      if (!cell.innerHTML) {
+        document.getElementById(cellId).innerHTML = mark;
+        mark = this.switchTurn(mark);
+        // this.grabTable();
+        this.updateTable(table);
+      }
+      if (this.win(mark)) console.log("You Won!!!!!!!!");
+      event.stopPropagation();
+    });
+  }
+  // grabTable() {
+  //   const myTable = table;
+  //   console.log("Table from grabTable", myTable);
+  //   return myTable;
+  // }
+  myTable() {
+    for (let row = 0; row < 3; row++) {
+      let tr = document.createElement("tr");
+      tr.setAttribute("id", row);
+      for (let col = 0; col < 3; col++) {
+        let td = document.createElement("td");
+        td.setAttribute("id", `[${row}, ${col}]`);
+        tr.appendChild(td);
+      }
+      table.appendChild(tr);
+    }
+    document.body.appendChild(table);
+  }
+  render() {
+    return (
+      <div>
+        {this.myTable()}
+        <Computer
+          win={this.win}
+          diagWin={this.diagWin}
+          colWin={this.colWin}
+          rowWin={this.rowWin}
+          grid={this.state.grid}
+          emptyCell={this.emptyCell}
+          PlayerMark={this.state.PlayerMark}
+          ComputerMark={this.state.ComputerMark}
+        />
+      </div>
+    );
+  }
+}
---
+++ .history/client/Board_20210107001436.js
@@ -0,0 +1,138 @@
+import React, { Component } from "react";
+import Computer from "./Computer";
+const table = document.createElement("table");
+export default class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      mycell: document.getElementsByTagName("td"),
+      grid: table,
+      PlayerMark: "O",
+      ComputerMark: "X",
+    };
+    this.placeMark = this.placeMark.bind(this);
+  }
+  componentDidMount() {
+    this.placeMark();
+  }
+
+  emptyCell(index) {
+    let cell;
+    if (index) {
+      let [r, c] = [index[0], index[1]];
+      cell = table.rows[r].cells[c];
+      return cell.innerHTML === "" ? true : false;
+    } else {
+      for (let i = 0; i < 3; i++) {
+        for (let n = 0; n < 3; n++) {
+          cell = table.rows[i].cells[n];
+          if (cell.innerHTML === "") {
+            return true;
+          }
+        }
+        return false;
+      }
+    }
+  }
+
+  rowWin(mark) {
+    for (let i = 0; i < 3; i++) {
+      for (let n = 0; n < 3; n++) {
+        const cell = table.rows[i].cells[n];
+        if (cell.innerHTML !== mark) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+  updateTable(cell) {
+    const newCell = this.state.mycell.map((element) => {
+      if (element.getAttribute("id") === cell.getAttribute("id")){
+        element.innerHTML === 
+      };
+      
+    });
+  }
+  colWin(mark) {
+    for (let i = 0; i < 3; i++) {
+      for (let n = 0; n < 3; n++) {
+        const cell = table.rows[n].cells[i];
+        if (cell.innerHTML !== mark) {
+          return false;
+        }
+      }
+      return true;
+    }
+  }
+  diagWin(mark) {
+    let n = 2;
+    for (let i = 0; i < 3; i++) {
+      const positiveCell = table.rows[i].cells[i];
+      const negativeCell = table.rows[i].cells[n];
+      if (positiveCell.innerHTML !== mark && negativeCell.innerHTML !== mark) {
+        return false;
+      }
+      n--;
+    }
+    return true;
+  }
+  win(mark) {
+    if (this.diagWin(mark) || this.colWin(mark) || this.rowWin(mark)) {
+      return true;
+    }
+  }
+
+  switchTurn(mark) {
+    if (mark === this.state.PlayerMark) {
+      return this.state.ComputerMark;
+    } else {
+      return this.state.PlayerMark;
+    }
+  }
+  placeMark() {
+    let mark = this.state.PlayerMark;
+    for (let i = 0; i < 3; i++) {
+      for (let n = 0; n < 3; n++) {
+        const cell = table.rows[i].cells[n];
+        cell.onclick = function () {
+          if (!cell.innerHTML) cell.innerHTML = mark;
+          event.stopPropagation();
+        };
+        this.updateTable(cell);
+        if (this.win(mark)) console.log("You Won!!!!!!!!");
+        mark = this.switchTurn(mark);
+      }
+    }
+  }
+  myTable() {
+    for (let row = 0; row < 3; row++) {
+      let tr = document.createElement("tr");
+      tr.setAttribute("id", row);
+      for (let col = 0; col < 3; col++) {
+        let td = document.createElement("td");
+        td.setAttribute("id", `[${row}, ${col}]`);
+        tr.appendChild(td);
+      }
+      table.appendChild(tr);
+    }
+    document.body.appendChild(table);
+  }
+  render() {
+    return (
+      <div>
+        {this.myTable()}
+        <Computer
+          win={this.win}
+          diagWin={this.diagWin}
+          colWin={this.colWin}
+          rowWin={this.rowWin}
+          grid={this.state.grid}
+          emptyCell={this.emptyCell}
+          PlayerMark={this.state.PlayerMark}
+          ComputerMark={this.state.ComputerMark}
+        />
+      </div>
+    );
+  }
+}
